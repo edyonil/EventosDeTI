a85339c7248286853e9a250a55b8ef575921ab18
NadsonSantos <nadsonsouza02@gmail.com>
2018-09-18 23:09:41 -0300

    sistema da API

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ var app = new Vue ({
     data:{
         titulo: 'title',
         items: [] ,
+        persons:[],
         search: '',
         lista : [] ,
         cadastro: {
@@ -19,22 +20,56 @@ var app = new Vue ({
             this.cadastro.name = '';
             this.cadastro.age = ''
         },
-
-        
+        // getInitialUsers () {
+        //     for (var i = 0; i < 5; i++) {
+        //         this.$http.get('https://randomuser.me/api/').then(response => {
+        //           this.persons.push(response.data.results[0]);
+        //         });
+        //     }
+        //   },
+        // beforeMount() {
+        //     this.getInitialUsers();
+        // },
+        scroll (persons) {
+            window.onscroll = () => {
+             let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+            if (bottomOfWindow) {
+            this.$http.get('https://randomuser.me/api/').then(response => {
+                persons.push(response.data.results[0]);
+                
+              });
+              console.log('teste')  
+          }
+        };
+      },
+    },
+    mounted() {
+      this.scroll(this.persons)
     },
     created : function(){
-        this.$http.get('dataserver.json').then(function(response){
-            console.log(response.data);
-            this.items = response.data;
-        });
-    },
-    computed : {
-        filteredItems: function() {
-            return this.items.filter((item)=> {
-                return item.description.match(this.search)
+        // this.$http.get('dataserver.json').then(function(response){
+        //     this.items = response.data;
+        // for (var i = 0; i < 5; i++) {
+        //     this.$http.get('dataserver.json').then(response => {
+        //       this.items.push(response.data);
+        //     });
+        // }
+         
+            for (var i = 0; i < 5; i++) {
+                this.$http.get('https://randomuser.me/api/').then(response => {
+                  this.persons.push(response.data.results[0]);
+                });
+            }
+          
+            //  this.getInitialUsers();
+        
+        },
+    // computed : {
+    //     filteredItems: function() {
+    //         return this.items.filter((item)=> {
+    //             return item.description.match(this.search)
                 
-            })
-        }
-    }
-    
+    //         })
+    //     }
+    // }
 })
