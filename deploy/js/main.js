370a8f7c3db5132e0b116a0b433f14f06f4c9286
NadsonSantos <nadsonsouza02@gmail.com>
2018-09-23 18:48:09 -0300

    deploy

---
+++ deploy/js/main.js
@@ -0,0 +1,59 @@
+var app = new Vue ({
+    el:'#app',
+    data:{
+        config : [],
+        items: [] ,
+        search: '',
+    },  
+    methods:{
+        getInitialUsers () {
+                self = this;
+                let x = 0;
+                this.$http.get('https://guarded-oasis-77929.herokuapp.com/api/event' ).then(response => {
+                    this.config = response.data;
+                    this.items = response.data.data.data;
+                    console.log(this.items);
+                    console.log(this.config);
+
+                });
+          },
+        beforeMount() {
+            this.getInitialUsers();
+        },
+        scroll (items) {
+            window.onscroll = () => {
+                let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+            if (bottomOfWindow) {
+            //         var self = this;
+            //         this.$http.get('https://guarded-oasis-77929.herokuapp.com/api/event?page=').then(response => {
+            //         self.items.push(response.data.data.data) ;
+            //         console.log(this.items)
+            //   });
+          }
+        };
+      },
+      buscar: function(){
+        alert(this.search);
+      }
+
+
+    },
+    mounted() {
+      this.scroll(this.items)
+    },
+    created : function(){          
+              this.getInitialUsers(); 
+        },
+    //
+    // filtro do Vue 
+    //
+    computed : {
+        filteredItems: function() {
+        //     return this.items.filter((item)=> {
+        //         return item.data.match(this.search)
+                
+        //     })
+        }
+    }
+})
+console.log('teste')  
